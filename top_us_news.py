b54734629aca298a11b48f2828afca1643efc800
Alex Klein <alex@facings.io>
2022-07-18 17:09:54 -0700

    make news story random in dashboard.

--- top_us_news.py
+++ top_us_news.py
@@ -1,11 +1,14 @@
 from gnews import GNews
-
+import random
 google_news = GNews()
 us_news = google_news.get_news('United States')
-title = us_news[0]["title"]
-url = us_news[0]["url"]
-publisher = us_news[0]["publisher"]
-date = us_news[0]["published date"]
-print(f"The top US Story is {title}")
-print(f"{url}")
-print(f"{date}")
+
+count = len(us_news)
+
+# Create a random number between 1 and count
+random_number = random.randint(1, count) 
+# Get the random news story
+random_story = us_news[random_number] 
+# Print the random story
+print(f"Story# {random_number} of {count} -  {random_story['title']}")
+print(random_story['description'])
